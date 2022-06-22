d56dff88d9d5d8a7ac1eee9c99221a20909e0e50
vcaracol <vcaracol@SurfacePro8.localdomain>
2022-06-22 12:16:07 -0700

    startup project

--- startup_project/lib/startup.rb
+++ startup_project/lib/startup.rb
@@ -1,7 +1,7 @@
 require "employee"
 class Startup
     attr_reader :name, :funding, :salaries, :employees
-    
+
     def initialize(name, funding, salaries)
         @name = name
         @funding = funding
@@ -9,33 +9,39 @@ class Startup
         @employees = []
     end
 
-    # def valid_title?(title)
-    #     @salaries.has_keys?(title)
-    # end
+    def valid_title?(title)
+        @salaries.has_key?(title)
+    end
 
-    # def >(another)
-    #     self.funding > another.funding
-    # end
+    def >(another)
+        self.funding > another.funding
+    end
 
-    # def hire(employee_name, title)
-    #     if self.valid_title?(title)
-    #         @employees << Employee.new(employee_name, title)
-    #     else
-    #         raise "title is invalid!"
-    #     end
-    # end
+    def hire(employee_name, title)
+        if self.valid_title?(title)
+            @employees << Employee.new(employee_name, title)
+        else
+            raise "title is invalid!"
+        end
+    end
 
-    # def size
-    #     @employees.length
-    # end
+    def size
+        @employees.length
+    end
 
-    # def pay_employee(employee)
-    #     payment = @salaries[employee.title]
-    #     if @funding >= pay
-    #         employee.pay(payment)
-    #         @funding -= payment
-    #     else
-    #         raise "not enough funding"
-    #     end
-    # end
+    def pay_employee(employee)
+        payment = @salaries[employee.title]
+        if @funding >= payment
+            employee.pay(payment)
+            @funding -= payment
+        else
+            raise "not enough funding"
+        end
+    end
+
+    def payday
+        @employees.each do |employee|
+            self.pay_employee(employee)
+        end
+    end
 end
